44d86eb313e2252c0cc7f89d82d33a3e86f07f38
Crisi <harmaharcri@hotmail.com>
2020-10-30 18:40:41 -0500

    Finished the ddl modeling sql
    *Finished the database modeling
    
    solves issue fk_code_trainer duplicated constraint

--- src/main/kotlin/co/uniquindio/edu/databaseConnection/DatabaseUtil.kt
+++ src/main/kotlin/co/uniquindio/edu/databaseConnection/DatabaseUtil.kt
@@ -9,15 +9,16 @@ class DatabaseUtil {
 
     /**
      * This method allows to create the database based
+     * @param sqlSentence sql sentence to create the database
      */
-    fun createDataBase(databaseDetails:String){
+    fun createDataBase(sqlSentence:String){
         connection?.getConnection()
         println("Success connection to mysql")
         var statement:Statement? = null
         try {
             statement = connection?.connection!!.createStatement()
-            statement.execute(databaseDetails)
-            println("Database created using ${databaseDetails}")
+            statement.execute(sqlSentence)
+            println("Database created using ${sqlSentence}")
 
         } catch (ex: SQLException) {
             ex.printStackTrace()
@@ -40,6 +41,10 @@ class DatabaseUtil {
         }
     }
 
+    /**
+     * This method allows to create a table based on the sql sentence
+     * @param sqlSentence to create the table
+     */
     fun createTable(sqlSentence:String){
         connection = null
         connection = Connection()
@@ -71,11 +76,26 @@ class DatabaseUtil {
             }
         }
     }
-    fun initDatabaseAndTables(){
 
+    /**
+     * This method allows to init the database and all the tables
+     */
+    fun initDatabaseAndTables(){
         createDataBase("CREATE DATABASE IF NOT EXISTS GEF_TEST;")
         createTable("CREATE TABLE IF NOT EXISTS Scholarship(code int NOT NULL AUTO_INCREMENT, description VARCHAR(255), PRIMARY KEY(code));")
         createTable("CREATE TABLE IF NOT EXISTS Member(code VARCHAR(11) NOT NULL, name VARCHAR(255), last_name VARCHAR(255), phone_number VARCHAR(255), email VARCHAR(255), PRIMARY KEY(code));")
-        //createTable("CREATE TABLE IF NOT EXISTS Member(code VARCHAR(11) NOT NULL, name VARCHAR(255), last_name VARCHAR(255), phone_number VARCHAR(255), email VARCHAR(255), PRIMARY KEY(code));")
+        createTable("CREATE TABLE IF NOT EXISTS Secretary(code VARCHAR(11) NOT NULL, name VARCHAR(255), last_name VARCHAR(255), phone_number VARCHAR(255), email VARCHAR(255), PRIMARY KEY(code));")
+        createTable("CREATE TABLE IF NOT EXISTS Trainer(code VARCHAR(11) NOT NULL, name VARCHAR(255), last_name VARCHAR(255), phone_number VARCHAR(255), email VARCHAR(255), PRIMARY KEY(code));")
+        createTable("CREATE TABLE IF NOT EXISTS PhysicalAssessment(code int NOT NULL AUTO_INCREMENT, date DATE, arms DECIMAL, legs DECIMAL, hips DECIMAL, height DECIMAL, weight DECIMAL, personal_goals LONGTEXT, code_trainer VARCHAR(11) NOT NULL, PRIMARY KEY(code), CONSTRAINT fk_code_trainer FOREIGN KEY(code_trainer) REFERENCES Trainer(code) ON UPDATE CASCADE);")
+        createTable("CREATE TABLE IF NOT EXISTS Membership(code int NOT NULL AUTO_INCREMENT, code_member VARCHAR(11) NOT NULL," +
+                "code_trainer VARCHAR(11) NOT NULL, code_secretary VARCHAR(11) NOT NULL, code_physical_assessment int NOT NULL, " +
+                "code_scholarship int NOT NULL, PRIMARY KEY(code), " +
+                "CONSTRAINT fk_code_trainer_member FOREIGN KEY(code_trainer) REFERENCES Trainer(code) ON UPDATE CASCADE," +
+                "CONSTRAINT fk_code_secretary FOREIGN KEY(code_secretary) REFERENCES Secretary(code) ON UPDATE CASCADE,"+
+                "CONSTRAINT fk_code_member FOREIGN KEY(code_member) REFERENCES Member(code) ON UPDATE CASCADE, "+
+                "CONSTRAINT fk_code_physical_assessment FOREIGN KEY(code_physical_assessment) REFERENCES PhysicalAssessment(code) ON UPDATE CASCADE, "+
+                "CONSTRAINT fk_code_scholarship FOREIGN KEY(code_scholarship) REFERENCES Scholarship(code) ON UPDATE CASCADE"+
+                ");")
+
     }
 }
